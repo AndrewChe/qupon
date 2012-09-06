be42cd8082303cfc180e0a148ceac39e6163e9ff
AndrewChe <mailtoandrew@ukr.net>
2012-09-06 11:44:02 +0300

    creating

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qap::Application.config.secret_token = 'a5e93866434cb5b8840acf3e039b0d999a68629d3fa8489e14cc0adea77596c3b57e83fa605b1a48227e420771c418b87a4648cf193bf45ebd6a590ccde899a0'
